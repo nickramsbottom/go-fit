0cf4b8aa75153f32223d6b3d870b0c5c3b363de0
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-26 15:54:36 +0100

    Read bytes with rawFileHeader

--- internal/headers/file.go
+++ internal/headers/file.go
@@ -10,8 +10,8 @@ import (
 	"os"
 )
 
-type FileHeader struct {
-	HeaderSize      int8
+type rawFileHeader struct {
+	Size            int8
 	ProtocolVersion int8
 	ProfileVersion  int16
 	DataSize        int32
@@ -19,22 +19,38 @@ type FileHeader struct {
 	Crc             uint16
 }
 
+type FileHeader struct {
+	Size            int8
+	ProtocolVersion int8
+	ProfileVersion  int16
+	DataSize        int32
+	FormatName      string
+	Crc             uint16
+}
+
 func NewFileHeader(f *os.File) *FileHeader {
-	header := FileHeader{}
+	rawHeader := rawFileHeader{}
 	data := io.ReadNextBytes(f, 14)
 	buffer := bytes.NewBuffer(data)
-	err := binary.Read(buffer, binary.LittleEndian, &header)
+	err := binary.Read(buffer, binary.LittleEndian, &rawHeader)
 	if err != nil {
 		log.Fatal("binary.Read failed", err)
 	}
 
-	return &header
+	return &FileHeader{
+		rawHeader.Size,
+		rawHeader.ProtocolVersion,
+		rawHeader.ProfileVersion,
+		rawHeader.DataSize,
+		string(rawHeader.FormatName[:]),
+		rawHeader.Crc,
+	}
 }
 
 func (h FileHeader) String() string {
 	return fmt.Sprintf(
 		"header size: %d\nprotocolVersion: %d\nprofileVersion: %d\ndataSize: %d\nparsed format: %s\ncrc: %d\n",
-		h.HeaderSize,
+		h.Size,
 		h.ProtocolVersion,
 		h.ProfileVersion,
 		h.DataSize,
