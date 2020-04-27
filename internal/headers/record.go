f358dd5de0c4079215b4945696a307f709d6f07e
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-27 21:36:01 +0100

    Rename MessageType to IsDefinitionMessage

--- internal/headers/record.go
+++ internal/headers/record.go
@@ -6,20 +6,20 @@ import (
 )
 
 type RecordHeader struct {
-	NormalHeader     bool
-	MessageType      bool
-	MessageSpecific  bool
-	LocalMessageType int
+	NormalHeader        bool
+	IsDefinitionMessage bool
+	MessageSpecific     bool
+	LocalMessageType    int
 }
 
 func NewRecordHeader(file *os.File) *RecordHeader {
 	headerByte := io.ReadNextBytes(file, 1)[0]
 
 	return &RecordHeader{
-		NormalHeader:     getBit(headerByte, 7),
-		MessageType:      getBit(headerByte, 6),
-		MessageSpecific:  getBit(headerByte, 5),
-		LocalMessageType: int(headerByte & 0xF),
+		NormalHeader:        getBit(headerByte, 7),
+		IsDefinitionMessage: getBit(headerByte, 6),
+		MessageSpecific:     getBit(headerByte, 5),
+		LocalMessageType:    int(headerByte & 0xF),
 	}
 }
 
